e1df1e36bbdef43fb4a8bbf2adbb09bbd6492b32
nora-srp <nora@srp.one>
2024-11-01 11:43:43 +0800

    Feat/add search proxy request schema (#15)
    * add search proxy schema
    
    * add search proxy schema

---
+++ favie_api_schema/schemas/search_product_proxy_schema.py
@@ -0,0 +1,26 @@
+
+from typing import Any, Dict, List, Optional
+
+from pydantic import BaseModel
+
+
+class SearchProductProxyRequest(BaseModel):
+    """
+    Search Product Proxy Request
+    """
+    query: str
+    top_k: Optional[int] = 1
+    source: Optional[str] = None
+    ranker: Optional[str] = None
+    site: Optional[str] = None
+    direct_link: Optional[str] = "False"
+
+
+
+class SearchProductProxyResponse(BaseModel):
+    """
+    Search Product Proxy Response
+    """
+    results: List[Dict[str, Any]]
+    
+    
